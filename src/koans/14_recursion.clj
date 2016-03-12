eb0e4e1c22714de78a2ce091600e5bdb2e89a100
Alan R. Soares <alanrsoars@gmail.com>
2016-03-12 18:37:13 +1300

    modules complete: functions/recursions

--- src/koans/14_recursion.clj
+++ src/koans/14_recursion.clj
@@ -3,21 +3,26 @@
 
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      false
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (if (empty? (rest coll))
+    coll
+    (cons (last coll)
+          (recursive-reverse (butlast coll)))))
 
 (defn factorial [n]
-  __)
+  (if (<= n 1)
+    n
+    (.abs (* n (factorial (- 1 n))))))
 
 (meditations
   "Recursion ends with a base case"
